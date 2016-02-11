ac0fc363c58c9b83593994e3a4e52f77d4628718
ashik <ashik@ashik>
2016-02-12 02:30:04 +0600

    Removing unnecessary components from index.jsp and Index.java controller.

--- src/java/com/controllers/Index.java
+++ src/java/com/controllers/Index.java
@@ -1,10 +1,5 @@
 package com.controllers;
 
-import com.util.ConnectToDatabase;
-import com.util.GetBeans;
-import java.sql.ResultSet;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +12,6 @@ public class Index {
     protected String doGet(Model model){
         
         try {
-            
-            ConnectToDatabase ct = (ConnectToDatabase) GetBeans.getBean("connectToDatabase");
-            ResultSet rs = ct.getResult("select * from bfs_data");
-            model.addAttribute("rs", rs);
             
             return "index";
         } catch (Exception e) {
